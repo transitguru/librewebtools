2920b75477aa423748169037ab4fce078e74a9a3
Michael Sypolt <msypolt@gmail.com>
2014-03-10 17:20:37 -0400

    Adjustments in filenames and instructions

--- includes/modules/core/settings.inc.php
+++ includes/modules/core/settings.inc.php
@@ -10,7 +10,7 @@
 function lwt_settings_load(){
   define('DB_NAME', 'librewebtools'); /**< The value should be the same as line 14 on ../sql/database.sql */
   define('DB_USER', 'lwt');           /**< The value should be the same as line 18 (before @'localhost') on ../sql/database.sql */
-  define('DB_PASS', 'LibreW38t00ls'); /**< The value should be the same as line 18 (after the PASSWORD */
+  define('DB_PASS', 'LibreW38t00ls'); /**< The value should be the same as line 18 (after the PASSWORD on ../sql/database.sql */
   return TRUE;
 }
   
