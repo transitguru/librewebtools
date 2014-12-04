1ed5af2bc67c4c848c15f83182e13ff2f4717673
Michael Sypolt <msypolt@gmail.com>
2014-12-04 09:28:14 -0500

    sed is a powerful tool, changing the db user to core, instead of lwt

--- includes/classes/settings.php
+++ includes/classes/settings.php
@@ -16,7 +16,7 @@ class coreSettings{
   public $host = 'localhost';       /**< The host for the database connection */
   public $type = 'mysql';           /**< Type of database */
   public $name = 'librewebtools';   /**< The database name for the application's data */
-  public $user = 'core';             /**< The username for the application's database user */
+  public $user = 'lwt';             /**< The username for the application's database user */
   public $pass = 'LibreW38t00ls';   /**< The password for the application's database user (It is recommended to change this from the default!!! */
   public $port = 3306;              /**< The port for the database connection */
   
