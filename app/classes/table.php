fc7a7e6410a71b8e4e5d32050b027757a45dfb80
Michael Sypolt <msypolt@tor.transitguru.info>
2018-09-09 03:09:39 +0000

    Started designing table definition array

--- app/classes/table.php
+++ app/classes/table.php
@@ -23,8 +23,8 @@ class Table{
     'smallint' => [ 'SMALLINT', 'SMALLINT', 'INTEGER'],
     'int' => [ 'INT', 'INT', 'INTEGER'],
     'bigint' => [ 'INT', 'INT', 'INTEGER'],
-    'serial' => [ 'INT PRIMARY KEY AUTO_INCREMENT', 'SERIAL', 'INTEGER PRIMARY KEY AUTO_INCREMENT'],
-    'bigserial' => [ 'SERIAL', 'BIGSERIAL', 'INTEGER PRIMARY KEY AUTO_INCREMENT'],
+    'serial' => [ 'INT PRIMARY KEY AUTO_INCREMENT', 'SERIAL', 'INTEGER PRIMARY KEY AUTOINCREMENT'],
+    'bigserial' => [ 'SERIAL', 'BIGSERIAL', 'INTEGER PRIMARY KEY AUTOINCREMENT'],
     'fixed' => [ 'NUMERIC', 'NUMERIC', 'NUMERIC'],
     'float' => [ 'FLOAT', 'REAL', 'REAL'],
     'double' => [ 'DOUBLE', 'DOUBLE PRECISION', 'REAL'],
@@ -38,5 +38,42 @@ class Table{
     'time' => [ 'TIME', 'TIME', 'TEXT'],
     'timex' => [ 'TIME', 'INTERVAL', 'TEXT']
   );
+
+  /**
+   * Create the table object
+   *
+   * @param array $table_defs Table definitions in the form of the array below
+   *
+   * @code
+   *   $table_defs = array(
+   *     'name' => 'table_name',
+   *     'comment' => 'Comment to include with the table',
+   *     'columns' => array(
+   *       [0] => array(
+   *         'name' => 'column_name',
+   *         'type' => 'varchar', //type in $rosetta_stone
+   *         'size' => 6,
+   *         'scale' => 2,        //used only with numeric for decimal scale
+   *         'null' => false,
+   *         'default' => 'default_value',
+   *         'comment' => 'Comment to include with the column'
+   *       ),
+   *     ),
+   *     'constraints' => array(
+   *       [0] => array(
+   *         'name' => 'constraint_name',
+   *         'type' => 'unique', // unique, primary, or foreign
+   *         'columns' => ['column1', 'column2'],
+   *         'ref_table' => 'foreign_key_referenced_table',
+   *         'ref_columns' => ['fk_ref_col1', 'fk_ref_col2'],
+   *         'delete' => 'cascade', // Use cascade, null, default, restrict, no
+   *         'update' => 'cascade'  // Use cascade, null, default, restrict, no
+   *       )
+   *     )
+   *   );
+   * @endcode
+   */
+  public function __construct($table_defs = array()){
+  }
 
 }
