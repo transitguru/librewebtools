82ec9b72994aff80c220406c6a6d246325159356
Michael Sypolt <msypolt@tor.transitguru.info>
2019-01-21 03:23:50 +0000

    Installer now adds data, using the json file

--- app/classes/installer.inc.php
+++ app/classes/installer.inc.php
@@ -120,7 +120,7 @@ class Installer{
           
           // Create the LWT database
           if ($db->db['type'] == 'mysql' || $db->db['type'] == 'pgsql'){
-            $sql = "CREATE DATABASE `{$db_name}` DEFAULT CHARACTER SET utf8";
+            $sql = 'CREATE DATABASE "' . $db_name . '" DEFAULT CHARACTER SET utf8';
             $db->write_raw($sql);
             if ($db->error > 0){
               $this->error = 1;
@@ -149,7 +149,7 @@ class Installer{
             $this->message = 'Error creating database';
           }
           else{
-            // Install the databases using the database.inc.php
+            // Install the databases using the install_db method
             $status = $this->install_db($post);
             $this->console .= "\n</pre>";
             if ($status == 0){
@@ -158,6 +158,7 @@ class Installer{
             }
             else{
               $this->message = "There was an error in the installation process!";
+              $this->error = $status;
             }
           }
         }
@@ -172,17 +173,19 @@ class Installer{
 
   /**
    * Installs the Database for the LWT
-   * TODO: Use table object to create the tables
    *
    * @return int error
    *
    */
   private function install_db($post){
+    //Load installer file
     $file = DOC_ROOT . '/app/json/installer.json';
     $json = file_get_contents($file);
 
+    //Keep track of raw SQL for building the tables
     $sql = (object) ['mysql' => '', 'pgsql' => '', 'sqlite' =>''];
 
+    //Build the tables from the installer file
     $table_builder = new LWT\Table();
     $object = json_decode($json);
     if (isset($object->tables) && is_array($object->tables)){
@@ -194,354 +197,55 @@ class Installer{
       }
     }
     else{
-      $this->console = "Error making tables";
+      $this->console .= "Error making tables\n";
       return 999;
     }
 
-    echo "<pre>" . $sql->mysql . $sql->pgsql . $sql->sqlite . "</pre>";
-    //TODO Put real tables in here
-
-/*
- Load JSON file with schema and data definitions for creating database
-*/
-
-
+    //Write the tables into the new Db (make sure to use the correct SQL type)
     $db = new Db();
-    $db->write_raw($sql);
+    $db->write_raw($sql->{$db->type});
     if ($db->error != 0){
       $db->error;
     }
-    // Set Date
+    // Set Date for 'created' fields
     $date = date('Y-m-d H:i:s');
 
-    $this->console .= "\nGroups\n";
-    
-    // Add root group at ID=0
-    $inputs = array(
-      'created' => $date,
-      'name' => 'Everyone',
-      'parent_id' => null,
-      'desc' => 'Root Level Group, Everyone!'
-    );
-    $db->write('groups', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
+    //Loop through inputs and write into the database
+    $table = '';
+    $inputs = array();
+    if (isset($object->data) && is_array($object->data)){
+      foreach($object->data as $data){
+        $table = $data->table;
+        $inputs = $data->inputs;
+        if ($table == 'users'){
+          // Add the Admin User using the User object
+          $user = new LWT\User(-1);
+          $user->login = $post['admin_user'];
+          $user->firstname = $inputs->firstname;
+          $user->lastname = $inputs->lastname;
+          $user->email = $post['admin_email'];
+          $user->roles = array(1 => [1]);
+          $user->groups = array(0 => [0]);
+          $user->write();
+          $this->console .= "{$user->error} \n";
+          if ($user->error){
+            return $user->error;
+          }
+          $user->setpassword($post['admin_pass']);
+        }
+        else{
+          if (isset($inputs->created)){
+            $inputs->created = $date;
+          }
+          $db->write($table, $inputs);
+        }
+      }
     }
-    $db->write_raw("UPDATE `groups` SET `id` = 0");
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write_raw("ALTER TABLE `groups` AUTO_INCREMENT=1");
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Continuing on the autonumbering the rest of the groups
-    $inputs['name'] = 'Unauthenticated';
-    $inputs['parent_id'] = 0;
-    $inputs['desc'] = 'Users who are not logged in, no user gets assigned this group';
-    $db->write('groups', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $inputs['name'] = 'Authenticated';
-    $inputs['desc'] = 'Basic Authenticated users';
-    $db->write('groups', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $auth_id = $db->insert_id;
-    // Subgroups of Authenticated
-    $inputs['parent_id'] = $auth_id;
-    $inputs['name'] = 'Internal';
-    $inputs['desc'] = 'Users within the organization';
-    $db->write('groups', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $inputs['name'] = 'External';
-    $inputs['desc'] = 'Users outside of the organization';
-    $db->write('groups', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    
-    
-    // Starting with role ID=0
-    $this->console .= "\nRoles\n";
-    $inputs = array(
-      'name' => 'Unauthenticated User',
-      'desc' => 'Users that are not logged in',
-      'created' => $date,
-    );
-    $db->write('roles', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write_raw("UPDATE `roles` SET `id` = 0");
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write_raw("ALTER TABLE `roles` AUTO_INCREMENT=1");
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Add the rest of the roles
-    $inputs['name'] = 'Administrator';
-    $inputs['desc'] = 'Administers website';
-    $db->write('roles', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $inputs['name'] = 'Authenticated User';
-    $inputs['desc'] = 'Basic user';
-    $db->write('roles', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Add the Admin User
-    $this->console .= "\nAdmin User\n";
-    $user = new User(-1);
-    $user->login = $post['admin_user'];
-    $user->firstname = 'Site';
-    $user->lastname = 'Administrator';
-    $user->email = $post['admin_email'];
-    $user->roles = array(1 => [1]);
-    $user->groups = array(0 => [0]);
-    $user->write();
-    $this->console .= "{$user->error} \n";
-    if ($user->error){
-      return $user->error;
-    }
-    $user->setpassword($post['admin_pass']);
-    
-    // Add the default theme
-    $this->console .= "\nThemes and Modules\n";
-    $inputs = array(
-      'type' => 'theme',
-      'core' => 1,
-      'code' => 'core',
-      'enabled' => 1,
-      'required' => 1,
-      'name' => 'Default Core',
-    );
-    $db->write('modules', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    $inputs['type'] = 'module';
-    $inputs['name'] = 'Core';
-    $db->write('modules', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Add the pages
-    $this->console .= "\nPages\n";
-    
-    // Add the homepage
-    $inputs = array(
-      'parent_id' => null,
-      'theme_id' => 1,
-      'user_id' => 1,
-      'url_code' => '/',
-      'title' => 'Home',
-      'app_root' => 0,
-      'core_page' => 1,
-      'ajax_call' => '',
-      'render_call' => '',
-      'created' => $date,
-    );
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write_raw("UPDATE `pages` SET `id` = 0 , `url_code` = ''");
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write_raw("ALTER TABLE `pages` AUTO_INCREMENT=1");
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => 0, 'group_id' => 0));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    
-    // Add the rest of the pages, starting with login
-    $inputs = array(
-      'parent_id' => 0,
-      'theme_id' => 1,
-      'user_id' => 1,
-      'url_code' => 'login',
-      'title' => 'Login',
-      'app_root' => 1,
-      'core_page' => 1,
-      'ajax_call' => 'core_auth_authentication',
-      'render_call' => 'core_auth_login',
-      'created' => $date,
-    );
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => 0));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // File download page
-    $inputs['url_code'] = 'file';
-    $inputs['title'] ='File Download';
-    $inputs['ajax_call'] = 'core_process_download';
-    $inputs['render_call'] = 'core_render_404';
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => 0));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Logout
-    $inputs['url_code'] = 'logout';
-    $inputs['title'] ='Logout';
-    $inputs['ajax_call'] = 'core_auth_logout';
-    $inputs['render_call'] = null;
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => 0));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Profile
-    $inputs['url_code'] = 'profile';
-    $inputs['title'] ='Profile';
-    $inputs['ajax_call'] = null;
-    $inputs['render_call'] = 'core_auth_profile';
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => $auth_id));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    //Password
-    $inputs['url_code'] = 'password';
-    $inputs['title'] ='Change Password';
-    $inputs['ajax_call'] = NULL;
-    $inputs['render_call'] = 'core_auth_password';
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => $auth_id));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
+    else{
+      $this->console = "Error writing data";
+      return 990;
     }
 
-    //Forgot password
-    $inputs['url_code'] = 'forgot';
-    $inputs['title'] ='Forgot Password';
-    $inputs['ajax_call'] = NULL;
-    $inputs['render_call'] = 'core_auth_forgot';
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => 0));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-
-    //Admin
-    $inputs['url_code'] = 'admin';
-    $inputs['title'] ='Administration';
-    $inputs['ajax_call'] = 'core_admin_ajax';
-    $inputs['render_call'] = 'core_admin_page';
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_roles', array('page_id' => $db->insert_id, 'role_id' => 1));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-
-    //Javascript CSS Loader
-    $inputs['url_code'] = 'scripts';
-    $inputs['title'] ='Not Found';
-    $inputs['ajax_call'] = 'core_send_scripts';
-    $inputs['render_call'] = 'core_render_404';
-    $db->write('pages', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    $db->write('page_groups', array('page_id' => $db->insert_id, 'group_id' => 0));
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
-    // Add Page content (just home page for now)
-    $inputs = array(
-      'page_id' => 0,
-      'user_id' => 1,
-      'created' => $date,
-      'title' => 'Home',
-      'content' => '<p>Welcome to LibreWebTools</p>',
-    );
-    $db->write('page_content', $inputs);
-    $this->console .= "{$db->error} \n";
-    if ($db->error != 0){
-      return $db->error;
-    }
-    
     return 0;
   }
 }
