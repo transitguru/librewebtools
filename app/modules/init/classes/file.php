ad41fe30b59f927b6d7f45def07adc5cf3a8eae2
Michael Sypolt <msypolt@tor.transitguru.info>
2019-01-31 20:42:49 +0000

    File downloads

--- app/modules/init/classes/file.php
+++ app/modules/init/classes/file.php
@@ -4,7 +4,7 @@ namespace LWT\Modules\Init;
  * @file
  * LibreWebTools Script Class
  *
- * Script Loader
+ * File Downloader
  *
  * @category   Request Handling
  * @package    LibreWebTools
@@ -13,7 +13,7 @@ namespace LWT\Modules\Init;
  * @license    http://www.gnu.org/licenses/gpl-3.0.txt
  * @version    @package_version@
  */
-Class Script Extends \LWT\Subapp{
+Class Download Extends \LWT\Subapp{
   /**
    * Fetches and and sends CSS or JS to the user
    */
@@ -28,18 +28,12 @@ Class Script Extends \LWT\Subapp{
     }
 
     // Check to see if the path is a valid file
-    $included = DOC_ROOT . '/app/' .$pathstring;
-    if (is_file($included) && (fnmatch('*.css', $pathstring) || (fnmatch('*.js', $pathstring)))){
+    $included = DOC_ROOT . '/files/' .$pathstring;
+    if (is_file($included) && (!fnmatch('.htaccess*', $pathstring))){
       //This is the only information that gets sent back!
       $size = filesize($included);
       $finfo = new \finfo();
       $type = $finfo->file($included, FILEINFO_MIME_TYPE);
-      if (fnmatch('*.css', $pathstring)){
-        $type = 'text/css';
-      }
-      elseif (fnmatch('*.js', $pathstring)){
-        $type = 'application/javascript';
-      }
       header('Pragma: ');         // leave blank to avoid IE errors
       header('Cache-Control: ');  // leave blank to avoid IE errors
       header('Content-Length: ' . $size);
